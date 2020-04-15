c5f93002210958704c2bb0cfa6747eafedafe2aa
Tram Do <dothuytram@gmail.com>
2020-04-15 00:15:54 -0400

    UC A1: generate a report with the books sold each month

--- src/ctrl/AnalyticsController.java
+++ src/ctrl/AnalyticsController.java
@@ -1,11 +1,23 @@
 package ctrl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import domain.Book;
+import domain.Cart;
+import domain.POItem;
 import service.BookService;
 import service.OrderService;
 
@@ -18,10 +30,28 @@ public class AnalyticsController {
 	@Autowired
 	OrderService orderService;
 	
+		
 	@GetMapping("/monthlyReport")
-	public ModelAndView montlyReport(@RequestParam("month")String month, @RequestParam("year")String year) {
-		ModelAndView mav = new ModelAndView("report");
-		return mav;
+	public String monthlyReport(@RequestParam(name="month", required=true)String month, @RequestParam(name="year", required=true)String year, Model model, HttpSession session) {
+		Map<Book, Integer> books = new HashMap<Book, Integer>();
+		List<POItem> orderedItems = orderService.getPOItemByDate(month, year);
+		for (POItem item: orderedItems) {
+			books.put(bookService.findById(item.getBid()), item.getQuantity());
+			
+		}
+		/*for (Map.Entry<Book, Integer> item : books.entrySet()) {
+			item.getValue()
+		}*/
+		//get book information
+		if (books.isEmpty()) {
+			model.addAttribute("message", "No results found.");
+		}
+		model.addAttribute("bookReport", books);
+
+		if (session.getAttribute("role").equals("Admin") )
+			return "report"; // Verify the User is a Admin and return the reports page
+		else 
+			return "404"; // Otherwise not authorized to view page.
 		
 	}
 	
